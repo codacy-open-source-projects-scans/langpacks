ae2d3e1a9be70cbcd01c2fbc140928b6fa228ca2
Alex Baranowski <aleksander.baranowski@yahoo.pl>
2023-07-25 09:19:32 +0200

    Frozen autocommit 2023-07-25 09:19:32.328742

--- tests/check-default-font-installed.sh
+++ tests/check-default-font-installed.sh
@@ -17,18 +17,18 @@ sudo dnf list installed|grep lohit-devanagari-fonts
 retval=$?
 echo $retval
 if [ $retval -ne 0 ]; then
-	echo "langpacks-core-hi failed to install lohit-devanagari-fonts as default font package"
+	echo "default-fonts-hi failed to install lohit-devanagari-fonts as default font package"
 else
-	echo "langpacks-core-hi installed with default font lohit-devanagari-fonts package"
+	echo "default-fonts-hi installed with default font lohit-devanagari-fonts package"
 fi
 
 # Check zh_CN language
-sudo dnf list installed|grep google-noto-sans-cjk-ttc-fonts
+sudo dnf list installed|grep google-noto-sans-cjk-vf-fonts
 retval=$?
 echo $retval
 if [ $retval -ne 0 ]; then
-        echo "langpacks-core-zh_CN failed to install google-noto-sans-cjk-ttc-fonts as default font package"
+        echo "default-fonts-zh_CN failed to install google-noto-sans-cjk-vf-fonts as default font package"
 else
-        echo "langpacks-core-zh_CN installed with default font google-noto-sans-cjk-ttc-fonts package"
+        echo "default-fonts-zh_CN installed with default font google-noto-sans-cjk-vf-fonts package"
 fi
 
